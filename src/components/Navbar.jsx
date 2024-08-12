e1668e3721edc96482e79bd80aebdc34f3b899a7
Akshay Pal <akshay.anu@gmail.com>
2024-08-12 20:55:48 +0100

    complete navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center'>
+            <h1 className='text-4xl font-bold'>AP</h1>
+        </div>
+        <div className='m-8 flex items center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub />
+        </div>
+    </nav>
   )
 }
 
