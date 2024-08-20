3350783ed5b648da27da9d3ccfea1f64a0ae32c2
Akshay Pal <akshay.pal@digital.homeoffice.gov.uk>
2024-08-20 13:54:16 +0100

    added resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className='mb-20 flex items-center justify-between py-6'>
             <div className='flex flex-shrink-0 items-center'>
-                <a href="../assets/resume.pdf" target="_blank"><h1 className='text-4xl font-bold'>AP</h1><span>⬆️ click to view my CV!</span></a>
+                <a href="./resume.pdf" target="_blank"><h1 className='text-4xl font-bold'>AP</h1><span>⬆️ click to view my CV!</span></a>
             </div>
             <div className='m-8 flex items center justify-center gap-4 text-4xl'>
                 <a href='https://www.linkedin.com/in/akshay-pal-4aa102183/' target='_blank'><FaLinkedin /></a>
